0e7639e4669a8c1f57ce6bf0e66517ba3c57a427
Huaming Rao <huaming.rao@gmail.com>
2013-04-23 09:52:32 -0500

    Initial Commit

---
+++ DataDispatcher/DataDispatcher.h
@@ -0,0 +1,13 @@
+//
+//  DataDispatcher.h
+//  DataDispatcher
+//
+//  Created by Huaming Rao on 4/23/13.
+//  Copyright (c) 2013 Huaming Rao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataDispatcher : NSObject
+
+@end
---
+++ DataDispatcher/DataDispatcher.m
@@ -0,0 +1,13 @@
+//
+//  DataDispatcher.m
+//  DataDispatcher
+//
+//  Created by Huaming Rao on 4/23/13.
+//  Copyright (c) 2013 Huaming Rao. All rights reserved.
+//
+
+#import "DataDispatcher.h"
+
+@implementation DataDispatcher
+
+@end
